8ff5e981010bdaaca9694c87c5852783fd0027ce
Develoopper <okhalfi2000@gmail.com>
2021-08-17 22:11:45 +0100

    .

--- root.js
+++ root.js
@@ -28,30 +28,30 @@ var rootNextOptions = {
 							nextOptions: {
 								values: 'input',
 								nextId: "question2",
-									nextLabel: "Question 2",
-									nextOptions: {
-										values: 'input',
-										nextId: 'question3',
-										nextLabel: 'Question 3',
-										nextOptions: [
-											{
-												value: 'Oui',
-												nextId: 'question4',
-												nextLabel: 'Question 4',
-												nextOptions: {
-													values: ['1', '2']
-												}
-											},
-											{
-												value: 'Non',
-												nextId: 'question5',
-												nextLabel: 'Question 5',
-												nextOptions: {
-													values: ['3', '4']
-												}
+								nextLabel: "Question 2",
+								nextOptions: {
+									values: 'input',
+									nextId: 'question3',
+									nextLabel: 'Question 3',
+									nextOptions: [
+										{
+											value: 'Oui',
+											nextId: 'question4',
+											nextLabel: 'Question 4',
+											nextOptions: {
+												values: ['1', '2']
 											}
-										]
-									},
+										},
+										{
+											value: 'Non',
+											nextId: 'question5',
+											nextLabel: 'Question 5',
+											nextOptions: {
+												values: ['3', '4']
+											}
+										}
+									]
+								},
 							},
 						},
 					},
