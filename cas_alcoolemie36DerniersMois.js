25617d64e93f0fe4c9897dd864745254ff23ca61
Develoopper <okhalfi2000@gmail.com>
2021-08-30 14:30:11 +0100

    ...

--- cas_alcoolemie36DerniersMois.js
+++ cas_alcoolemie36DerniersMois.js
@@ -1,27 +1,37 @@
 var cas_alcoolemie36DerniersMois = {
-  nextId: "controleAlcoolemieSuiteA36DerniersMois",
-	nextLabel: "Contrôle effectue suite à : ",
+	nextId: "dateDernierAlcoolemie36DerniersMois",
+	nextLabel: "Date du dernier test d'alcoolémie",
 	nextOptions: {
-		values: ["Controle", "routier", "sinistre"],
-		nextId: "isRefusTestAlcoolemie36DerniersMois",
-		nextLabel: "Refus de faire le test après alcoolémie ?",
+		nextId: "tauxAlcoolemie",
+		nextLabel: "Taux d'alcoolémie",
 		nextOptions: {
-      values: ["Non", "Oui"],
-      nextId: "isDelitDeFuiteAlcoolemie36DerniersMois",
-      nextLabel: "Délit de fuite après alcoolémie ?",
-      nextOptions: {
-        values: ["Non", "Oui"],
-        nextId: "controleAlcoolemieMethode36DerniersMois",
-        nextLabel: "Teste effectue avec",
-        nextOptions: {
-          values: ["prise de sang", "ethylotest"],
-          nextId: "tauxAlcoolemie",
-          nextLabel: "Taux alcoolémie",
-          nextOptions: {
-            values: 'text'
-          },
-        },
-      },
-    },
+			values: "text",
+			nextId: "controleAlcoolemieSuiteA36DerniersMois",
+			nextLabel: "Contrôle effectue suite à",
+			nextOptions: {
+				values: ["Controle", "routier", "sinistre"],
+				nextId: "isRefusTestAlcoolemie36DerniersMois",
+				nextLabel: "Refus de faire le test après alcoolémie ?",
+				nextOptions: {
+					values: ["Non", "Oui"],
+					nextId: "isDelitDeFuiteAlcoolemie36DerniersMois",
+					nextLabel: "Délit de fuite après alcoolémie ?",
+					nextOptions: {
+						values: ["Non", "Oui"],
+						nextId: "controleAlcoolemieMethode36DerniersMois",
+						nextLabel: "Teste effectue avec",
+						nextOptions: {
+							values: ["prise de sang", "ethylotest"],
+							nextId: "tauxAlcoolemie",
+							nextLabel: "Taux alcoolémie",
+							nextOptions: {
+								values: "text",
+                
+							},
+						},
+					},
+				},
+			},
+		},
 	},
-}
+};
